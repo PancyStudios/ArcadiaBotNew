f664b515489aed40c95f5fc4e5d62645d19dec49
Turbiis <162517611+Turbiis@users.noreply.github.com>
2024-08-01 14:46:40 -0600

    chore: Update server initialization in index.ts
    Update server initialization in `src/index.ts` to use the `serverPort` environment variable as the port number. This change allows for more flexibility in configuring the server port and ensures consistency with other parts of the codebase.

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import { ErrorHandler } from './handlers/Errors'
 import { ArcadiaDb } from './database'
 import { ServerWeb } from './handlers/Server'
 
+const port = (process.env.serverPort || 3000) as number
+
 export const clientExtend = new ExtendedClient()
 export const errorManager = new ErrorHandler()
 export const db = new ArcadiaDb()
-export const server = new ServerWeb(process.env.serverPort || 3000)
+export const server = new ServerWeb(port)
 
 db.init()
 clientExtend.start()
