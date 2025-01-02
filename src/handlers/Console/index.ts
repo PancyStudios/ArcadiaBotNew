c808ad929767f08b8e00742f01a8d5999cdc764d
Turbiis <162517611+Turbiis@users.noreply.github.com>
2025-01-01 23:46:04 -0600

    Opciones experimentales

--- src/handlers/Console/index.ts
+++ src/handlers/Console/index.ts
@@ -1,24 +1,22 @@
 import { version as DiscordJSVersion} from "discord.js"
 import { version as TypeScriptVersion } from "typescript"
+import { cachedDataVersionTag } from "v8"
 import { totalmem, freemem} from "os"
 
 export default class ConsoleHandler {
     constructor() {
-        console.log('Iniciando consola')
+        console.log(`Iniciando consola`)
         // Aqui al iniciar la consola que muestre algunas especificaciones del sistema, es nodejs y discordjs
 
-        console.log('Sistema operativo:', process.platform)
-        console.log('Version de Node:', process.version)
-        console.log('Version de Discord.js:', DiscordJSVersion)
-        console.log('Version de TypeScript:', TypeScriptVersion)
+        console.info(`Sistema operativo: ${process.platform}`)
+        console.info(`Version de Node: ${process.version}`)
+        console.info(`Version de Discord.js: ${DiscordJSVersion}`)
+        console.info(`Version de TypeScript: ${TypeScriptVersion}`)
 
         // Que muestre especificaciones del sistema, como la cantidad de memoria que esta usando el bot
-        console.log('CPU:', process.cpuUsage())
-        console.log('Memoria usada:', process.memoryUsage())
-        console.log('Memoria total:', totalmem)
-        console.log('Memoria libre:', freemem)
-
-
-        console.log('Consola lista')
+        console.info(`CPU: ${process.cpuUsage().user}%`)
+        console.info(`Memoria usada: ${process.memoryUsage().heapUsed}`)
+        console.info(`Memoria total: ${totalmem}`)
+        console.info(`Memoria libre: ${freemem}`)
     }
 }
